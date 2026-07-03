c5aa5591ccd39715c441d5c6f24787fcdc6bce09
406-9-charupat <54221@yupparaj.ac.th>
2026-07-03 10:50:38 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My Digital Profile")
+
+st.header("แนะนำตัว")
+st.write("นาย จารุภัทร อรุณสิทธิ์")
+st.write("ชื่อเล่น โอ๊ต")
+st.write("ชั้น 4/6 เลขที่ 9")
+
+st.header("เป้าหมายที่อยากทำในอนาคต")
+st.write("อยากเรียนด้านกีฬา")
+st.write("อยากเป็นนักกีฬาบาสทีมชาติ")
